76c14b6b66f614a5c581461504e695d4a1c7088a
Ivan Medvedev <sartek158@gmail.com>
2019-05-22 23:45:42 +0300

    Обновил проект

---
+++ sandbox/test/java/ru/stqa/pft/sanbox/SquareTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sanbox;
+
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+  @Test
+  public void TestArea() {
+    Square s = new Square(5);
+    assert  s.area() == 25;
+  }
+}
