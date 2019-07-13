41b137acf49372abdcce3b215da6989b2a037018
Ivan Medvedev <sartek158@gmail.com>
2019-07-13 23:26:21 +0300

    Добавил конструцию try в методы записи и  чтения данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -55,28 +55,28 @@ public class ContactDataGenerator {
     }
 
     private void  saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts) {
-            writer.write(String.format("%s:%s:%s:%s:%s:%s:%s:%s:%s\n", contact.getFirstname(), contact.getLastname(), contact.getFirstPhone(), contact.getSecondPhone(), contact.getThirdPhone(), contact.getAddress(), contact.getFirstEmail(), contact.getSecondEmail(), contact.getThirdEmail()));
+        try(Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s:%s:%s:%s:%s:%s:%s:%s:%s\n", contact.getFirstname(), contact.getLastname(), contact.getFirstPhone(), contact.getSecondPhone(), contact.getThirdPhone(), contact.getAddress(), contact.getFirstEmail(), contact.getSecondEmail(), contact.getThirdEmail()));
+            }
         }
-        writer.close();
     }
 
     private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
         XStream xStream = new XStream();
         xStream.processAnnotations(ContactData.class);
         String xml = xStream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
 
