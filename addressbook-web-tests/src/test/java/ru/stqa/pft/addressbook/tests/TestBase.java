58062660f1b2e1319ec81d2118b3da0987509e51
Ivan Medvedev <sartek158@gmail.com>
2019-07-21 21:21:10 +0300

    Добавил сравнение обьектов из UI и DB

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,14 +3,23 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -37,6 +46,27 @@ public class TestBase {
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m, Object[] p) {
         logger.info("Stop test " + m.getName() + " with parametrs " + Arrays.asList(p));
+    }
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
+
+    public void verifyContactListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Contacts dbContacts = app.db().contacts();
+            Contacts uiContacts = app.contact().all();
+            assertThat(uiContacts, equalTo(dbContacts.stream()
+                    .map((g) -> new ContactData().withId(g.getId()).withFirstName(g.getFirstname()).withLastName(g.getLastname()).withFirstEmail(g.getFirstEmail()).withSecondPhone(g.getSecondPhone()).withThirdPhone(g.getThirdPhone())
+                            .withAddress(g.getAddress()).withSecondEmail(g.getSecondEmail()).withThirdEmail(g.getThirdEmail()))
+                    .collect(Collectors.toSet())));
+        }
     }
 
 }
