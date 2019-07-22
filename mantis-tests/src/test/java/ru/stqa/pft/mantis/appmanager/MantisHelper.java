7270118104475f1236b0b6b4f67f598d142ccb59
Ivan Medvedev <sartek158@gmail.com>
2019-07-22 21:29:50 +0300

    Установил Mantis и подготовил новый проект для нового учебного приложения

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MantisHelper.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+public class MantisHelper extends HelperBase {
+
+    public MantisHelper(WebDriver wd) {
+        super(wd);
+    }
+
+
+}
