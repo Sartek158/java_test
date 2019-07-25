389e41d1ffa2ab771498eed69a2a7ca737b7e2a4
Ivan Medvedev <sartek158@gmail.com>
2019-07-25 15:42:25 +0300

    Небольшой рефактор

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double y;
+  public double x;
+
+  public Point(double y, double x) {
+    this.y = y;
+    this.x = x;
+  }
+
+  public double distance(Point p2) {
+    return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
+  }
+}
